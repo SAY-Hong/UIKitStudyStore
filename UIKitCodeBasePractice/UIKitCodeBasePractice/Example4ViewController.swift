c6f78446f47db2ddf81581be606b6cf2dfb11166
SAY-Hong <bobseahee@gmail.com>
2023-11-14 10:12:36 +0900

    오토레이아웃 예제 4번

--- UIKitCodeBasePractice/UIKitCodeBasePractice/Example4ViewController.swift
+++ UIKitCodeBasePractice/UIKitCodeBasePractice/Example4ViewController.swift
@@ -2,7 +2,7 @@
 //  Example4ViewController.swift
 //  UIKitCodeBasePractice
 //
-//  Created by 홍세희 on 2023/11/13.
+//  Created by 홍세희 on 2023/11/14.
 //
 
 import UIKit
@@ -11,36 +11,19 @@ class Example4ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let redView = UIView()
-        redView.backgroundColor = UIColor.red
-        redView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(redView)
-        
-        let greenView = UIView()
-        greenView.backgroundColor = UIColor.green
-        greenView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(greenView)
-        
-        
-        let constraints = [
-            redView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            redView.topAnchor.constraint(equalTo: self.view.topAnchor),
-            redView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            redView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1/3),
-            
-            greenView.leadingAnchor.constraint(equalTo: self.view.centerXAnchor),
-            greenView.topAnchor.constraint(equalTo: self.view.topAnchor),
-            greenView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            greenView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1/3),
-            greenView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1/2),
-            
-            
-        ]
-        
-        NSLayoutConstraint.activate(constraints)
-    }
-}
 
-#Preview {
-    Example4ViewController()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
