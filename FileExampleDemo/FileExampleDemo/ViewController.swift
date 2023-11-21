214f91ea9f64bad5bb072976222bf95156e08f8a
SAY-Hong <bobseahee@gmail.com>
2023-11-21 11:45:49 +0900

    file and directory example 01 ~ 05
    -todo: 하나하나 다시 이해하기

--- FileExampleDemo/FileExampleDemo/ViewController.swift
+++ FileExampleDemo/FileExampleDemo/ViewController.swift
@@ -20,10 +20,150 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         // 화면 로딩하면 이미 있는 파일인지 체크하는 코드 작성하기
         //checkFile()
-        example1()
+        //example1()
+        example3()
         
     }
-
+    //MARK: 05 - 단어의 빈도수를 계산하고 저장
+    func example5() {
+        let fileManager = FileManager.default
+        
+        do {
+            let documentPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].path
+            let sourceFile = documentPath + "/" + "datafile.dat"
+            let destinationFile = documentPath + "/" + "frequency.txt"
+            
+            // 단어의 빈도수를 계산하고 저장하는 코드
+            let data = try Data(contentsOf: URL(fileURLWithPath: sourceFile))
+            if let text = String(data: data, encoding: .utf8) {
+                
+                let words = text.split(separator: " ").map { $0.lowercased() }
+                var frequency = [String:Int]()
+                for word in words {
+                    frequency[word, default: 0] += 1
+                }
+                var result = ""
+                for (word, count) in frequency {
+                    result += "\(word):\(count)\n"
+                }
+                
+                if let resultData = result.data(using: .utf8) {
+                    try resultData.write(to: URL(fileURLWithPath: destinationFile))
+                }
+            }
+            
+            // 변경된 파일 확인
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    //MARK: 04 - 숫자들의 합계와 평균을 구하고 저장하는 코드
+    func example4() {
+        let fileManager = FileManager.default
+        
+        do {
+            let documentPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].path
+            let sourceFile = documentPath + "/" + "datafile.dat"
+            let destinationFile = documentPath + "/" + "summary.txt"
+            
+            // 숫자들의 합계와 평균을 구하고 저장하는 코드
+            let data = try Data(contentsOf: URL(fileURLWithPath: sourceFile))
+            if let text = String(data: data, encoding: .utf8) {
+                
+                let numbers = text.split(separator: ",").compactMap { Int($0) }
+                let sum = numbers.reduce(0, +)
+                let average = Double(sum) / Double(numbers.count)
+                let summary = "합계: \(sum)\n평균: \(average)"
+                
+                if let summaryData = summary.data(using: .utf8) {
+                    try summaryData.write(to: URL(fileURLWithPath: destinationFile))
+                }
+            }
+            
+            // 변경된 파일 확인
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    //MARK: 03 - 특정 파일에 있는 텍스트를 역순으로 바꾸어 새로운 파일에 저장하기
+    func example3() {
+        let fileManager = FileManager.default
+        
+        do {
+            let documentPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].path
+            let sourceFile = documentPath + "/" + "datafile.dat"
+            let destinationFile = documentPath + "/" + "reversed.txt"
+            
+            // 텍스트를 역순으로 바꾸어 저장하는 코드
+            let data = try Data(contentsOf: URL(fileURLWithPath: sourceFile))
+            if let text = String(data: data, encoding: .utf8) {
+                let reversedText = String(text.reversed())
+                if let reverseData = reversedText.data(using: .utf8) {
+                    try reverseData.write(to: URL(fileURLWithPath: destinationFile))
+                }
+            }
+            
+            // 변경된 파일 확인
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    //MARK: 02 - 새로운 디렉토리 생성 후 파일 복사
+    func example2() {
+        let fileManager = FileManager.default
+        
+        do {
+            let sourcePath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].path
+            let destinationPath = sourcePath + "/" + "BackUp"
+            //            let destinationPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathExtension("BackUp").path
+            print(sourcePath)
+            print(destinationPath)
+            
+            //폴더 만들기
+            try fileManager.createDirectory(atPath: destinationPath, withIntermediateDirectories: true)
+            let files = try fileManager.contentsOfDirectory(atPath: sourcePath)
+            
+            for file in files {
+                // 파일을 복사
+                let filePath = sourcePath + "/" + file
+                let fileExtension = (file as NSString).pathExtension.lowercased()
+                if fileExtension == "dat" {
+                    let destinationFilePath = destinationPath + "/" + file
+                    try fileManager.copyItem(atPath: filePath, toPath: destinationFilePath)
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    //MARK: 01 - 파일 이름과 크기 출력
+    func example1() {
+        let fileManager = FileManager.default
+        let currentPath = fileManager.currentDirectoryPath
+        
+        do {
+            let files = try fileManager.contentsOfDirectory(atPath: currentPath)
+            for file in files {
+                // 파일 이름과 크기를 출력하는 코드를 작성하세요.
+                let filePath = currentPath + "/" + file
+                
+                let attributions: NSDictionary = try fileManager.attributesOfItem(atPath: filePath) as NSDictionary
+                let fileSize = attributions["NSFileSize"] as! UInt64 // 바이트 단위
+                print("\(file) : \(fileSize) bytes")
+                //print(file)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
     //사용자가 앱을 시작할 때마다 데이터 파일이 존재하는지 확인
     //파일이 존재하는 경우 앱에서 콘텐츠를 읽고 텍스트 필드에 표시하기
     //사용자가 이전에 텍스트를 저장한 적이 없을 경우 파일이 생성되지 않음
@@ -41,37 +181,6 @@ class ViewController: UIViewController {
                 //textBox.text = dataString as? String
                 textBox.text = dataString as String?
             }
-        }
-    }
-    
-    func example1() {
-        let fileManager = FileManager.default
-        let currentPath = fileManager.currentDirectoryPath
-        
-        do {
-            let files = try fileManager.contentsOfDirectory(atPath: currentPath)
-            for file in files {
-                // 파일 이름과 크기를 출력하는 코드를 작성하세요.
-                print(file)
-                print(file)
-            }
-        } catch {
-            print(error)
-        }
-    }
-    
-    func example2() {
-        let fileManager = FileManager.default
-        let currentPath = fileManager.currentDirectoryPath
-        do {
-            let files = try fileManager.contentsOfDirectory(atPath: currentPath)
-            for file in files {
-                // 파일 이름과 크기를 출력하는 코드를 작성하세요.
-                print(file)
-                print(file)
-            }
-        } catch {
-            print(error)
         }
     }
     
